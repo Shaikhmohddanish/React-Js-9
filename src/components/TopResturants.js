9d69c0dfc788b0c95e295bae8ec61eb4cfbcb83b
Shaikhmohddanish <danishshaikh2365@gmail.com>
2025-09-17 22:14:48 +0530

    Implement search functionality for restaurants in TopResturants component

--- src/components/TopResturants.js
+++ src/components/TopResturants.js
@@ -17,12 +17,30 @@ function TopResturants() {
       json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants;
     setListOfResturants(restaurants);
   };
+
+  const setUpdatedResturants = (searchText) => {
+    const filteredResturants = listOfResturants.filter((resturant) =>
+      resturant.info.name.toLowerCase().includes(searchText.toLowerCase())
+    );
+    setListOfResturants(filteredResturants);
+  }
+  // let searchInputData = "";
+  const [searchInputData,setSearchInputData] = useState("");
+  // const [searchData, setSearchData] = useState("");
   return listOfResturants.length === 0 ? (
     <>
       <Shimmer />
     </>
   ) : (
     <>
+      {/* <div className="search-container">
+        <input type="text" value={searchData} className="search-input" placeholder="Search for restaurants..." onChange={(e) => setSearchData(e.target.value)} />
+        <button className="search-button" onClick={() => {setUpdatedResturants(searchData)}}>Search</button>
+      </div> */}
+      <div>
+        <input type="text" value={searchInputData} placeholder="Search Restaurants" onChange={(event) => setSearchInputData(event.target.value)}/>
+        <button onClick={()=>{setUpdatedResturants(searchInputData)}}>Search</button>
+      </div>
       <div className="top-resturants">
         {listOfResturants.map((resturant) =>
           <ResturantCard data={resturant} key={resturant.info.id} />
